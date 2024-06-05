187831173b3d1470fade63ec49eafff17f4bed30
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-06-04 20:44:10 -0400

    Upgrade to 4o model

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ export default {
 				apiKey: env.OPENAI_API_KEY,
 			});
 			const response = await openai.chat.completions.create({
-				model: "gpt-3.5-turbo",
+				model: "gpt-4o",
 				messages: [
 					{
 						role: "user",
